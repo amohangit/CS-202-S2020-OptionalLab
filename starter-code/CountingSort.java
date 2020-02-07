5746c92404eea47431f4fa93205e7869a268f3e1
amohan <amohan@ALDENV8075.local>
2020-02-07 14:28:49 -0500

    first commit

---
+++ starter-code/CountingSort.java
@@ -0,0 +1,63 @@
+/*
+ADD HONOR CODE HERE.
+*/
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.ArrayList;
+public class CountingSort {
+    public static int[] prepareData(int[] patients){
+        /*
+        Data is prepared by inserting random values 
+        between 1 and 1000. Data items may be assumed to 
+        be unique. 
+        Please refer to lab spec for the problem definiton.
+        */
+        // what is our range?
+        int max = 1000;
+        // create instance of Random class
+        Random randomNum = new Random();
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i=0; i<patients.length; i++) {
+            int ids = randomNum.nextInt(max);
+            list.add(ids);
+        }
+        Collections.shuffle(list);
+    
+        for (int i=0; i<patients.length; i++) {
+            patients[i] = list.get(i);
+        }
+    
+
+        return patients;
+    }  
+    public static int[] sortArray(int[] patient_ids){
+        /*
+        Add your logic below to sort the array.*/
+        
+
+
+        return patient_ids;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter the no of patients:");
+        Scanner scan = new Scanner(System.in);
+        int patients = scan.nextInt();
+        int[] patient_ids = new int[patients]; 
+        int[] patients_populated = prepareData(patient_ids);
+
+        
+        /* Implement the sortArray method, so as to 
+        get the correct results.*/
+        int[] sorted_array = sortArray(patients_populated);
+        
+        /* The two lines below will print the output. 
+        Do not uncomment these lines.  */
+        System.out.println("Unsorted:\t" + Arrays.toString(patients_populated));   
+        System.out.println("Sorted:\t" + Arrays.toString(sorted_array));   
+        
+    }   
+}
